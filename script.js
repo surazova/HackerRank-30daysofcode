46a3986e58dab662cf2acce278696719aeea28a7
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-05 11:36:29 +0000

    0/30 JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-    // Declare second integer, double, and String variables.
-    var i2;
-    var d2;
-    var s2;
-    // Read and save an integer, double, and String to your variables.
-    i2 = parseInt(readLine());
-    d2 = parseFloat(readLine());
-    s2 = readLine();
-    // Print the sum of both integer variables on a new line.
-    console.log(i + i2);
-    // Print the sum of the double variables on a new line.
-    console.log((d + d2).toFixed(1));
-    // Concatenate and print the String variables on a new line
-    // The 's' variable above should be printed first.
-    console.log(s + s2);
+function processData(inputString) {
+    // This line of code prints the first line of output
+        console.log("Hello, World.");
+
+    // Write the second line of output that prints the contents of 'inputString' here.
+
+var inputString = "HackerRank is the best!"
+console.log(inputString);
+}
