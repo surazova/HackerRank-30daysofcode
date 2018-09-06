97ff06094674a4e77b323ee749e48a50d6ed9feb
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-06 17:15:17 +0000

    2/30 JavaScript

---
+++ day2.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the solve function below.
+function solve(mealCost, tipPercent, taxPercent) {
+
+let tip = mealCost * tipPercent / 100;
+let tax = mealCost * taxPercent / 100;
+ 
+let totalCost = Math.round(mealCost + tax + tip);
+ 
+console.log("The total meal cost is " + totalCost + " dollars.");
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine());
+
+    const tip_percent = parseInt(readLine(), 10);
+
+    const tax_percent = parseInt(readLine(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
